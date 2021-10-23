99759bd571b90744ff082f205d8023524f07d094
Cryotechnic <cryogen65@gmail.com>
2021-10-23 04:37:27 -0400

    fixed incorrect plugin definition

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,7 +152,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google`,
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-99652921-3', // FIXME: Evaluate if this works
       },
