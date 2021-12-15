647fcfe9fe34faf14895b3cca641cfee8aaea6c0
CryoTechnic <cryogen65@gmail.com>
2021-12-14 20:12:42 -0500

    removed tags that are no longer in use in order to prevent graphql query crashes

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -170,16 +170,8 @@ const ArchivePage = ({ location, data }) => {
             <tbody>
               {projects.length > 0 &&
                 projects.map(({ node }, i) => {
-                  const {
-                    date,
-                    github,
-                    external,
-                    ios,
-                    android,
-                    title,
-                    tech,
-                    company,
-                  } = node.frontmatter;
+                  const { date, github, external, ios, android, title, tech, company } =
+                    node.frontmatter;
                   return (
                     <tr key={i} ref={el => (revealProjects.current[i] = el)}>
                       <td className="overline year">{`${new Date(date).getFullYear()}`}</td>
@@ -242,6 +234,7 @@ ArchivePage.propTypes = {
 
 export default ArchivePage;
 
+// frontmatter :{ios, android} were removed due to compilation errors. Readd if needed
 export const pageQuery = graphql`
   {
     allMarkdownRemark(
@@ -256,8 +249,6 @@ export const pageQuery = graphql`
             tech
             github
             external
-            ios
-            android
             company
           }
           html
