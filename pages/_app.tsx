b867b267b86deec7493577ecb3c2145bfbf1b789
Ron Friedman <cryogen65@gmail.com>
2023-03-19 17:01:36 -0400

    fixed dark mode toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,42 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import { MantineProvider, Title } from '@mantine/core'
+import { useState } from 'react';
+import NextApp, { AppProps, AppContext } from 'next/app';
+import { getCookie, setCookie } from 'cookies-next';
+import Head from 'next/head';
+import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 
-export default function App(props: AppProps) {
-  const { Component, pageProps } = props
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+    setCookie('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
+  };
 
   return (
     <>
       <Head>
-        <title>Page Title</title>
-        <meta name="description" content="Page description" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Ron Friedman</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme: 'light',
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+          <Component {...pageProps} />
+          <Notifications />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
+
+App.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await NextApp.getInitialProps(appContext);
+  return {
+    ...appProps,
+    colorScheme: getCookie('mantine-color-scheme', appContext.ctx) || 'dark',
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,13 @@ import { Switch, Group, useMantineColorScheme, useMantineTheme } from '@mantine/
 import { IconSun, IconMoonStars } from '@tabler/icons-react';
 
 export default function Home() {
-  // const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  // const theme = useMantineTheme();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
   return (
     <Group position="center" my={30}>
     <Switch
-      // checked={colorScheme === 'dark'}
-      // onChange={() => toggleColorScheme()}
+      checked={dark}
+      onChange={() => toggleColorScheme()}
       size="lg"
       onLabel={<IconSun color="white" size="1.25rem" stroke={1.5} />}
       offLabel={<IconMoonStars color="black" size="1.25rem" stroke={1.5} />}
