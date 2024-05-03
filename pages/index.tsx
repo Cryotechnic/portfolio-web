7134390f464bea091d1205a567f816c5e26d120c
Ron Friedman <cryogen65@gmail.com>
2024-05-03 01:40:38 -0400

    testing stuff with code that doesn't work...yet
    retrieve repos from github api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,34 @@
-import { Modal, Group, Button } from '@mantine/core';
+import { Group } from '@mantine/core';
 import { HeaderSimple } from '../components/nav/HeaderSimple';
 import { CarouselProjects } from '../components/body/CarouselProjects';
-import { BadgeCard } from '../components/body/BadgeCard';
+// import type { InferGetStaticPropsType, GetStaticProps } from 'next';
+// import { BadgeCard } from '../components/body/BadgeCard';
 // import { useDisclosure } from '@mantine/hooks';
 // import { , Group, Button } from '@mantine/core';
+
+// type Repo = {
+//   name: string;
+//   description: string;
+//   html_url: string;
+//   homepage: string;
+//   stargazers_count: number;
+//   language: string;
+//   owner: {
+//     login: string;
+//     avatar_url: string;
+//   };
+// };
+
+// export const getStaticProps = (async (context) => {
+//   const response = await fetch('https://api.github.com/users/Cryotechnic/repos');
+//   const repos: Repo[] = await response.json();
+
+//   return {
+//     props: {
+//       repos,
+//     },
+//   };
+// }) satisfies GetStaticProps<{ repos: Repo[] }>;
 
 export default function Home() {
   return (
@@ -14,23 +39,33 @@ export default function Home() {
         { link: '#work', label: '03. Work' },
         { link: '#contact', label: '04. Contact' }
       ]} />
-  <CarouselProjects
-  />
-  {/* <Group position="center">
-    <BadgeCard 
-      image="https://raw.githubusercontent.com/neon-nyan/CollapseLauncher-Page/main/images/NewBanner2022.webp"
-      title="Project 1"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet nisl. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet nisl."
-      country="United States"
-      badges={[
-        { emoji: '🏆', label: 'Winner' },
-        { emoji: '🏆', label: 'Winner' },
-        { emoji: '🏆', label: 'Winner' },
-        { emoji: '🏆', label: 'Winner' },
-        { emoji: '🏆', label: 'Winner' },
-      ]}
-    />
-  </Group> */}
+    <CarouselProjects />
   </Group>
   );
 }
+// export default function Home({ repos }: InferGetStaticPropsType<typeof getStaticProps>) {
+//   // return repos.map((repo) => (
+//   //   <div key={repo.name}>
+//   //     <h1>{repo.name}</h1>
+//   //     <p>{repo.description}</p>
+//   //     <a href={repo.html_url} target="_blank">View on GitHub</a>
+//   //   </div>
+//   // ));
+//   return (
+//     <Group position="right" my={30}>
+//       <HeaderSimple links={[
+//           { link: '#about', label: '01. About' },
+//           { link: '#experience', label: '02. Experience' },
+//           { link: '#work', label: '03. Work' },
+//           { link: '#contact', label: '04. Contact' }
+//         ]} />
+//       {/* <CarouselProjects
+//     repos.map((repo) => (
+//       <div key={repo.name}>
+//         <h1>{repo.name}</h1>
+//         <p>{repo.description}</p>
+//         <a href={repo.html_url} target="_blank">View on GitHub</a>
+//       </div>
+//       /> */}
+//     </Group>
+//   )
